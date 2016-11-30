a4f77b977e8e5e907d8b58b17b27208afe712676
Jonathan Skelton <jskelton@newage.local>
2016-11-30 14:38:45 -0600

    Initial commit

---
+++ preventUnauthorizedInterceptor.service.js
@@ -0,0 +1,30 @@
+(function PrevenUnauthInterceptorFactory(module) {
+
+  function PreventUnauthorizedInterceptor($rootScope, $q) {
+    return {
+      request: function interceptorRequest(config) {
+
+        var isHtmlOrCss = (config.url.indexOf('.html') || config.url.indexOf('.css')) > -1;
+        var isOAuthOrLogin = config.url.indexOf('oauth') > -1 || config.url.indexOf('login') > -1;
+        if (isHtmlOrCss || isOAuthOrLogin) {
+          return config;
+        }
+
+        var deferred = $q.defer();
+        $rootScope.$on('checkTokenExpirationComplete', function checkTokenExpirationComplete(data) {
+          deferred.resolve(config);
+        });
+
+        $rootScope.$broadcast("requestIntercepted");
+        return deferred.promise;
+      }
+    };
+  }
+
+  module.factory('preventUnauthorizedInterceptor', [
+    "$rootScope",
+    "$q",
+    PreventUnauthorizedInterceptor
+  ]);
+
+})(angular.module("angular.HX.token.interceptors"));
---
+++ unauthorizedInterceptor.service.js
@@ -0,0 +1,40 @@
+(function UnauthInterceptorFactory(module) {
+
+  function UnauthorizedInterceptor(
+    $rootScope,
+    $q,
+    jwtHelper,
+    httpBuffer,
+    localStorageService
+  ) {
+
+    return {
+      responseError: function responseError(rejection) {
+        var deferred = $q.defer();
+        if (!rejection.config.ignoreAuthModule) {
+          switch (rejection.status) {
+            case 401:
+              httpBuffer.append(rejection.config, deferred);
+              $rootScope.$broadcast("event:auth-refreshRequired", rejection.status);
+              break;
+            case 403:
+              $rootScope.$broadcast("event:auth-forbidden", rejection.status);
+              break;
+          }
+        }
+        deferred.reject(rejection);
+        return deferred.promise;
+      }
+    };
+  }
+
+  module.factory('unauthorizedInterceptor', [
+    "$rootScope",
+    "$q",
+    "jwtHelper",
+    "httpBuffer",
+    "localStorageService",
+    UnauthorizedInterceptor
+  ]);
+
+})(angular.module("angular.HX.token.interceptors"));
